abee4a775f33d040585fead870151d750bbe1772
Diana Garbuz <GDI.1999@yandex.ru>
2022-07-15 13:21:19 +0300

    stub page

--- src/App.js
+++ src/App.js
@@ -6,75 +6,29 @@ import AboutPage from './component/AboutPage/AboutPage';
 import SongsPage from './component/SongsPage/SongsPage';
 import PoemsPage from './component/PoemsPage/PoemsPage';
 import ContactsPage from './component/ContactsPage/ContactsPage';
+import StubPage from './component/StubPage/StubPage';
 import Header from './component/shared/Header/Header';
 import Footer from './component/shared/Footer/Footer';
 
 function App() {
   return (
     <div className="app">
-      <Header />
+      {/* <Header /> */}
 
-      <React.Fragment>
+      <StubPage />
+
+      {/* <React.Fragment>
         <Routes>
           <Route path="/" element={<AboutPage />} />
           <Route path="/songs" element={<SongsPage />} />
           <Route path="/poems" element={<PoemsPage />} />
           <Route path="/contacts" element={<ContactsPage />} />
-          <Route
-            path="/test"
-            element={
-              <div>
-                <div className="test-container">
-                  <div className="poems-page__shadow-base"></div>
-                  <div className="poems-page__shadow-up"></div>
-                  <p className="poems-page__poem-text">
-                    Никто, ничто, никак, ничем
-                    <br />
-                    Не вдохновляли меня прежде.
-                    <br />
-                    Но Вы, право, особенны,
-                    <br />
-                    Красивы – без или в одежде.
-                    <br />
-                    <br />
-                    Ведь рядом с Вами в тот же миг
-                    <br />
-                    Я обретаю вдохновенье.
-                    <br />
-                    Не скрою я, что у меня
-                    <br />
-                    Есть к Вам огромное влеченье.
-                    <br />
-                    <br />
-                    Я к Вам, ей-богу, буду впредь
-                    <br />
-                    Всегда, всю жизнь с огромным сердцем.
-                    <br />
-                    Я рад бы был, если бы Вы
-                    <br />
-                    Пошли б со мной, с шикарным перцем.
-                    <br />
-                    <br />
-                    Для Вас готов на много я -<br />
-                    Вы просветите хоть рентгеном.
-                    <br />
-                    Я честный парень, без гнилья,
-                    <br />
-                    С большой душой, с не малым …<br />
-                    <br />
-                    17/01/2021
-                  </p>
-                  <div className="poems-page__shadow-down"></div>
-                </div>
-              </div>
-            }
-          />
 
           <Route element={<AboutPage />} />
         </Routes>
-      </React.Fragment>
+      </React.Fragment> */}
 
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/component/AboutPage/AboutPage.js
+++ src/component/AboutPage/AboutPage.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
 import './AboutPage.scss';
+import { Transition } from 'react-transition-group';
 import mainPhoto from '../../public/mainPhoto.png';
+import mainPhotoTablet from '../../public/mainPhoto-tablet.png';
+import mainPhotoMobile from '../../public/mainPhoto-mobile.png';
+
 import mainPhoto2 from '../../public/mainPhoto2.png';
+import mainPhoto2Tablet from '../../public/mainPhoto2-tablet.png';
+import mainPhoto2Mobile from '../../public/mainPhoto2-mobile.png';
 import Sergey from '../../public/Sergey.svg';
 import Shmidt from '../../public/Shmidt.svg';
 import number20 from '../../public/number20.svg';
@@ -11,12 +17,28 @@ import focus from '../../public/focus.svg';
 const AboutPage = () => {
   const [blockScroll, setBlockScroll] = useState(false);
   const [step, setStep] = useState(0);
+  const [animItem, setAnimItem] = useState(-1);
+
+  const [prevStep, setPrevStep] = useState(7);
+  const [mobile, setMobile] = useState(() => {
+    if (window.innerWidth <= 600) {
+      return true;
+    }
+    return false;
+  });
+
+  const timeAnim = 2000;
+
+  console.log('step-', step, ' prevStep-', prevStep);
 
   const scrollHandler = (event) => {
     if (!blockScroll) {
       if (event.deltaY < 0 && step !== 8) {
         console.log('Scroll!', step);
-        setStep((value) => value + 1);
+        setStep((value) => {
+          setPrevStep(value);
+          return value + 1;
+        });
         setBlockScroll(true);
         setTimeout(() => {
           setBlockScroll(false);
@@ -24,7 +46,10 @@ const AboutPage = () => {
       }
       if (event.deltaY > 0 && step !== 0) {
         console.log('Scroll!', step);
-        setStep((value) => value - 1);
+        setStep((value) => {
+          setPrevStep(value);
+          return value - 1;
+        });
         setBlockScroll(true);
         setTimeout(() => {
           setBlockScroll(false);
@@ -40,78 +65,265 @@ const AboutPage = () => {
         scrollHandler(event);
       }}
     >
-      <div className="about-page__item-1">
-        <p className="about-page__text">
-          musician.poet.
-          <br />
-          composer.
-        </p>
-        <div className="about-page__scroll-container">
-          <p className="about-page__scroll-text">scroll</p>
-          <div className="about-page__scroll">
-            <div className="about-page__scroll-pointer"></div>
+      <Transition
+        in={step === 0}
+        timeout={timeAnim}
+        //  moutOnEnter
+        unmountOnExit
+      >
+        {(state) => (
+          <div
+            className={`about-page__block-1 ${
+              state ? `about-page__block-1--${state}` : ''
+            }`}
+          >
+            <p className="about-page__text">
+              musician.poet.
+              <br />
+              composer.
+            </p>
+            <div className="about-page__scroll-container">
+              <p className="about-page__scroll-text">scroll</p>
+              <div className="about-page__scroll">
+                <div className="about-page__scroll-pointer"></div>
+              </div>
+            </div>
           </div>
+        )}
+      </Transition>
+
+      <div className="about-page__tablet-scroll-container">
+        <div className="about-page__scroll">
+          <div
+            className={`about-page__tablet-scroll-line about-page__tablet-scroll-line--${step}`}
+          ></div>
+          <div className="about-page__tablet-scroll-pointer"></div>
         </div>
-      </div>
-      <div className="about-page__item-2">
-        <img src={Sergey} className="about-page__first-name " alt="Sergey" />
-        <img src={Shmidt} className="about-page__full-name " alt="Shmidt" />
-      </div>
-      <img src={number20} alt="20" className="about-page__item-4" />
-      <img src={practice} alt="practice" className="about-page__item-5" />
-      <div>
-        <img src={focus} alt="focus" className="about-page__item-9" />
-      </div>
-      <div className="about-page__item-3">
-        <p className="about-page__text  about-page__item-6">
-          <span>I've been playing guitar for</span>
-          <br /> <span>over</span> twenty years.
-        </p>
-        <p className="about-page__text  about-page__item-7">
-          <span>
-            After graduating from music school,
-            <br /> I've dedicated few years to
-          </span>{' '}
-          play songs
-          <br />
-          <span>from famous bands and musicians.</span>
-        </p>
-        <p className="about-page__text  about-page__item-8">
-          <span>
-            In the following years my focus
-            <br /> was{' '}
-          </span>{' '}
-          to master{' '}
-          <span>
-            drums, electric
-            <br /> and bass guitar
-          </span>
-        </p>
-        <p className="about-page__text  about-page__item-10">
-          <span>Since 2020 I've been working on</span>
-          <br /> writing my own{' '}
-          <span>
-            songs and poems.
-            <br /> I think you’ll enjoy it!
-          </span>
-        </p>
-        <button className="about-page__button">Listen</button>
-        <div className="about-page__scroll-container">
-          <p className="about-page__scroll-text">scroll</p>
-          <div className="about-page__scroll">
-            <div className="about-page__scroll-line"></div>
-            <div className="about-page__scroll-pointer"></div>
-          </div>
-        </div>
+        <p className="about-page__scroll-text">scroll</p>
       </div>
 
-      <div className="about-page__img">
-        <img src={mainPhoto} alt="avatar" />
+      <Transition
+        in={step === 0}
+        timeout={timeAnim}
+        //  moutOnEnter
+        unmountOnExit
+      >
+        {(state) => (
+          <div
+            className={`about-page__block-2 ${
+              state ? `about-page__block-2--${state}` : ''
+            }`}
+          >
+            <img
+              src={Sergey}
+              className="about-page__block-2-first-name"
+              alt="Sergey"
+            />
+            <img
+              src={Shmidt}
+              className="about-page__block-2-full-name"
+              alt="Shmidt"
+            />
+          </div>
+        )}
+      </Transition>
+
+      <Transition
+        in={step === 1 || step === 2}
+        timeout={timeAnim}
+        //  moutOnEnter
+        unmountOnExit
+      >
+        {(state) => (
+          <p
+            className={`about-page__block-4 ${
+              state ? `about-page__block-4--${state}-${step}` : ''
+            }`}
+          >
+            20
+          </p>
+        )}
+      </Transition>
+
+      <Transition
+        in={step === 3 || step === 4}
+        timeout={timeAnim}
+        //  moutOnEnter
+        unmountOnExit
+      >
+        {(state) => (
+          <p
+            className={`about-page__block-5 ${
+              state ? `about-page__block-5--${state}-${step}` : ''
+            }`}
+          >
+            {mobile ? (
+              'practice'
+            ) : (
+              <>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;prac-
+                <br />
+                tice
+              </>
+            )}
+          </p>
+        )}
+      </Transition>
+      <Transition
+        in={step === 5 || step === 6}
+        timeout={timeAnim}
+        //  moutOnEnter
+        unmountOnExit
+      >
+        {(state) => (
+          <p
+            className={`about-page__block-19 ${
+              state ? `about-page__block-19--${state}-${step}` : ''
+            }`}
+          >
+            focus
+          </p>
+        )}
+      </Transition>
+
+      {/* <img src={practice} alt="practice" className="about-page__item-5" /> */}
+      {/* <div>
+        <img src={focus} alt="focus" className="about-page__item-9" />
+      </div> */}
+
+      <p
+        className={`about-page__text-block  about-page__block-6 about-page__block-6--${prevStep}-${step}`}
+      >
+        <span>I've been playing guitar for </span>
+        <span>over</span> twenty years.
+      </p>
+
+      <p
+        className={`about-page__text-block  about-page__block-7 about-page__block-7--${prevStep}-${step}`}
+      >
+        <span>
+          After graduating from music school, I've dedicated few years to
+        </span>{' '}
+        play songs <span>from famous bands and musicians.</span>
+      </p>
+
+      <p
+        className={`about-page__text-block  about-page__block-8 about-page__block-8--${prevStep}-${step}`}
+      >
+        <span>In the following years my focus was </span> to master{' '}
+        <span>drums, electric and bass guitar</span>
+      </p>
+
+      <div
+        className={` about-page__block-9 about-page__block-9--${prevStep}-${step}`}
+      >
+        <p className="about-page__text-block  ">
+          <span>Since 2020 I've been working on</span> writing my own{' '}
+          <span>songs and poems. I think you’ll enjoy it!</span>
+        </p>
+        <button className="about-page__button">Listen</button>
       </div>
-      <div className="about-page__img-2">
+
+      <Transition
+        in={step >= 2 && step <= 9}
+        timeout={{
+          appear: 1000,
+          enter: 1000,
+          exit: 1000,
+        }}
+        //  moutOnEnter
+        unmountOnExit
+      >
+        {(state) => (
+          <div className={`about-page__block-3 about-page__block-3--${state}`}>
+            <div className="about-page__scroll-container">
+              <p className="about-page__scroll-text">scroll</p>
+              <div className="about-page__scroll">
+                <div
+                  className={`about-page__scroll-line about-page__scroll-line--${
+                    step + 1
+                  }`}
+                ></div>
+                <div
+                  className={`about-page__scroll-pointer about-page__scroll-pointer--${
+                    step + 1
+                  }`}
+                ></div>
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+
+      {/* <Transition
+        in={step === 0}
+        timeout={timeAnim}
+        //  moutOnEnter
+      >
+        {(state) => (
+          <picture
+            className={`about-page__img-block-1 ${
+              state ? `about-page__img-block-1--${state}` : ''
+            }`}
+          >
+            <source srcSet={mainPhoto} media="(min-width: 1041px)" />
+            <source srcSet={mainPhotoTablet} media="(min-width: 600px)" />
+
+            <img srcSet={mainPhotoMobile} alt="avatar" />
+          </picture>
+        )}
+      </Transition> */}
+      {/* <div className="about-page__img-2">
         <img src={mainPhoto2} alt="avatar" />
-      </div>
-      <div className="about-page__circle"></div>
+      </div> */}
+      {/* <div className="about-page__circle"></div> */}
+
+      <Transition in={step === 0} timeout={timeAnim}>
+        {(state) => (
+          <div
+            className={`about-page__circle-1 ${
+              state ? `about-page__circle-1--${state}` : ''
+            }`}
+          ></div>
+        )}
+      </Transition>
+
+      <Transition in={step === 0} timeout={timeAnim}>
+        {(state) => (
+          <picture
+            className={`about-page__img-block-1 ${
+              state ? `about-page__img-block-1--${state}` : ''
+            }`}
+          >
+            <source srcSet={mainPhoto} media="(min-width: 1041px)" />
+            <source srcSet={mainPhotoTablet} media="(min-width: 600px)" />
+
+            <img srcSet={mainPhotoMobile} alt="avatar" />
+          </picture>
+        )}
+      </Transition>
+      <Transition
+        in={step === 8}
+        timeout={{
+          appear: 1000,
+          enter: 1000,
+          exit: timeAnim,
+        }}
+        unmountOnExit
+      >
+        {(state) => (
+          <picture
+            className={`about-page__img-block-2 ${
+              state ? `about-page__img-block-2--${state}` : ''
+            }`}
+          >
+            <source srcSet={mainPhoto2} media="(min-width: 1024px)" />
+            <source srcSet={mainPhoto2Tablet} media="(min-width: 600px)" />
+
+            <img srcSet={mainPhoto2Mobile} alt="avatar" />
+          </picture>
+        )}
+      </Transition>
     </div>
   );
 };
